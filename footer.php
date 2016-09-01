a090f691119f40935f9be144f2f4e2e5d64bb7c8
Marvin Gibbs <c.marv.gibbs@gmail.com>
2016-09-01 14:30:36 -0700

    header.php -
    Replace navigation with static site menu and connected to WP Theme menu
    
    Needed to connect bootstrap and while there added google fonts and HTML5 shiv declaration
    
    Fixed nav bar img logo path to connect to image in assets
    
    screenshot.php -
    
    Replaced image with Mike Cramer Eagle image to be displayed with Theme image
    
    style.css -
    
    Copied custom.css static site styles at the bottom of this file as this is the file that  WP uses instead. Removed styles in this file that will be replaced with bootstrap files and the styles copied from custom.css. Fixed path disconnect to images. Added a rule for .logged-in to adjust the top to display the menu. Finally added a couple of @media rules to adjust the menu for screen resizing.
    
    footer.php -
    
    Copied footer html from static page. Also set up javascript and font links and from static page.

--- footer.php
+++ footer.php
@@ -11,18 +11,85 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'madisonriverranch' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'madisonriverranch' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'madisonriverranch' ), 'madisonriverranch', '<a href="http://marvgibbs.us" rel="designer">Marv Gibbs</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
 
 <?php wp_footer(); ?>
 
+	<!-- !FOOTER
+	=============================================================================== -->
+	<footer>
+		<div class="container">
+			<div class="col-sm-3">
+				<p><a href="/"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/mrr-logo.png" alt="Madison River Ranch"></a></p>
+			</div> <!-- .col -->
+			<div class="col-sm-6">
+				<nav>
+					<ul class="list-unstyled list-inline">
+						<li><a href="">Home</a></li>
+						<li><a href="">Board</a></li>
+						<li><a href="">Archives</a></li>
+						<li><a href="">Contact</a></li>
+					</ul>
+				</nav>
+			</div> <!-- .col -->
+			<div class="col-sm-3">
+				<p class="pull-right">&copy; 2016 Madison River Ranch</p>
+			</div> <!-- .col -->
+		</div> <!-- .container -->	
+	</footer>
+	
+	<!-- !MODAL
+	=============================================================================== -->
+	<div class="modal fade" id="myModal">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">
+						<span aria-hidden="true">&times;</span>
+						<span class="sr-only">Close</span>
+					</button>
+					<h4 class="modal-title" id="myModalLabel">
+						<i class="fa fa-envelope"></i> Subscribe to our Mailing List
+					</h4>
+				</div> <!-- modal-header -->
+				
+				<div class="modal-body">
+					<p>Simply enter you name and email! And you will receive all association newsletters and mailings</p>
+					
+					<form class="form-inline" role="form">
+						<div class="form-group">
+							<label class="sr-only" for="subscribe-name">Your name</label>
+							<input type="text" class="form-control" id="subscribe-name" placeholder="Your name">
+						</div> <!-- form-group -->
+						<div class="form-group">
+							<label class="sr-only" for="subscribe-email">and your email</label>
+							<input type="text" class="form-control" id="subscribe-email" placeholder="and your email">
+							<input type="submit" class="btn btn-danger" value="Subscribe">
+						</div> <!-- form-group -->	
+					</form> <!-- form -->
+					
+					<hr>
+					
+					<p><small>By providing your email you consent to receiving occasional emails &amp; newsletters. <br>No Spam. Just good stuff. We respect your privacy &amp; you may unsubscribe at any time.</small></p>
+				</div> <!-- modal-body -->
+				
+				
+			</div> <!-- modal-content -->
+		</div> <!-- modal-dialog -->
+	</div> <!-- modal -->
+
+	<!-- BOOTSTRAP CORE JAVASCRIPT
+		Place at the end of the document so the pages load faster!
+	===============================================================================  -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" type="text/javascript"></script>
+	
+	<script src="<?php bloginfo('template_directory'); ?>/assets/js/jquery-2.1.1.min.js" type="text/javascript"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/assets/js/main.js" type="text/javascript"></script>
+	
+	<!-- Type kit fonts -->
+	<script src="//use.typekit.net/nax0mea.js"></script>
+	<script>try{Typekit.load({ async: true });}catch(e){}</script>	
+		
 </body>
 </html>
