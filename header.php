aef90a8f49ccc00ddf3e33de1faec8aaa5ab2584
Marvin Gibbs <c.marv.gibbs@gmail.com>
2016-09-01 11:41:56 -0700

    Replace navigation with static site menu and connected to WP Theme menu
    Needed to connect bootstrap and while there added google fonts and HTML5 shiv declaration

--- header.php
+++ header.php
@@ -17,34 +17,65 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/bootstrap.min.css">
+<!-- FontAwesom Icons -->
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/font-awesome/css/font-awesome.min.css">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+
 <?php wp_head(); ?>
+
+<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'madisonriverranch' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'madisonriverranch' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+	<!-- !HEADER
+	=============================================================================== -->		
+	<header class="site-header" role="banner">
+		
+		<!-- NAVEBAR
+		=============================================================================== -->
+		<div class="navbar-wrapper">
+			<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a href="/" class="navbar-brand"><img src="assets/img/mrr-logo.png" alt="Madison River Ranch"></a>
+					</div><!-- .navbar-header -->
+					
+					
+					<!-- If the menu (WP admin area) is not set, then the "menu_class" is applied to "container".
+						 In other words, it overwrites the "ontainer_class". 
+						 Ref: http://wordpress.org/support/topic/wp_nav_menu-menu_class-usage-bug?replies=4 -->
+					<?php 
+						
+						wp_nav_menu( array (
+							
+							'theme_location' 	=> 'primary',
+							'container'			=> 'nav',
+							'container_class'	=> 'navbar-collapse collapse',
+							'menu_class'		=> 'nav navbar-nav navbar-right'
+								
+						)); 
+						
+					?>
+					
+				</div><!-- .container -->
+			</div><!-- .navbar -->
+		</div><!-- .navbar-wrapper -->
+	</header>
