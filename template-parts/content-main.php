75968d7c12733fed320bc451cf204808a59221af
Marvin Gibbs <c.marv.gibbs@gmail.com>
2016-09-02 14:33:12 -0700

    header.php -
    Replace navigation with static site menu and connected to WP Theme menu
    
    Needed to connect bootstrap and while there added google fonts and HTML5 shiv declaration
    
    Fixed nav bar img logo path to connect to image in assets
    
    screenshot.php -
    
    Replaced image with Mike Cramer Eagle image to be displayed with Theme image
    
    style.css -
    
    Copied custom.css static site styles at the bottom of this file as this is the file that  WP uses instead. Removed styles in this file that will be replaced with bootstrap files and the styles copied from custom.css. Fixed path disconnect to images. Added a rule for .logged-in to adjust the top to display the menu. Finally added a couple of @media rules to adjust the menu for screen resizing.
    
    footer.php -
    
    Copied footer html from static page. Also set up javascript and font links and from static page.
    
    page-home.php
    
    Separated sections into individual content pages and loaded them.
    
    page-board.php
    
    Created new board page to display board members
    
    content-board.php
    
    This is the actual board page dynamic html using Board Members Custom Post types
    
    content-main.php
    
    The is the actual main page section dynamic html using Custom Fields

---
+++ template-parts/content-main.php
@@ -0,0 +1,15 @@
+<!-- !MAIN
+=============================================================================== -->		
+<section id="main-section">
+	<article>
+		<div class="container clearfix">
+			<div class="row">
+				<div class="col-sm-offset-5 col-sm-7 main-section-text">
+					<h1><?php bloginfo('name'); ?></h1>
+					<p class="lead"><?php bloginfo('description'); ?></p>
+				</div> <!-- .col -->
+			</div> <!-- .row -->
+		</div> <!-- .container -->
+	</article>
+	
+</section> <!-- #main-section -->
