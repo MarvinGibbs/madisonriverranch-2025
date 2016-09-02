6f57a56bbe3e57c3625def7054dc8e60d3c845fe
Marvin Gibbs <c.marv.gibbs@gmail.com>
2016-09-02 14:31:36 -0700

    header.php -
    Replace navigation with static site menu and connected to WP Theme menu
    
    Needed to connect bootstrap and while there added google fonts and HTML5 shiv declaration
    
    Fixed nav bar img logo path to connect to image in assets
    
    screenshot.php -
    
    Replaced image with Mike Cramer Eagle image to be displayed with Theme image
    
    style.css -
    
    Copied custom.css static site styles at the bottom of this file as this is the file that  WP uses instead. Removed styles in this file that will be replaced with bootstrap files and the styles copied from custom.css. Fixed path disconnect to images. Added a rule for .logged-in to adjust the top to display the menu. Finally added a couple of @media rules to adjust the menu for screen resizing.
    
    footer.php -
    
    Copied footer html from static page. Also set up javascript and font links and from static page.
    
    page-home.php
    
    Separated sections into individual content pages and loaded them.
    
    page-board.php
    
    Created new board page to display board members
    
    content-board.php
    
    This is the actual board page dynamic html using Board Members Custom Post types

---
+++ template-parts/content-board.php
@@ -0,0 +1,47 @@
+<?php 
+
+$board_title= get_field( 'board_title' );
+	
+?>
+
+<!-- !BOARD
+=============================================================================== -->		
+<section id="board">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<h2><?php echo $board_title; ?></h2>
+
+				<?php 	$loop = new WP_QUERY( array( 	'post_type' => 'board_member', 
+														'orderby' => 'post_id', 
+														'order' => 'ASC' ) ); 
+										
+				while ( $loop->have_posts() )  : $loop->the_post(); ?>
+
+					<!-- MEMBERS -->
+					<div class="row member">
+						<div class="col-sm-4">
+							<?php if ( has_post_thumbnail() ) {
+									the_post_thumbnail( array ( 200, 200 )); 
+								}
+							?>
+						</div> <!-- .col -->
+						<div class="col-sm-8">
+							<blockquote>
+								<?php the_title(); ?>
+								<cite>
+								<?php the_content(); ?>
+								</cite>
+							</blockquote>
+						</div> <!-- .col -->							
+					</div> <!-- .row -->
+								
+				<?php endwhile; 
+					
+				wp_reset_query();
+				?>
+
+			</div> <!-- .col -->
+		</div> <!-- .row -->
+	</div> <!-- .container -->
+</section> <!-- #board -->
