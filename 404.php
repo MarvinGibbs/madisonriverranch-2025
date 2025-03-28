bc1dbf4e5bbd80e726627ac75011138b7f9139e3
Marvin Gibbs <c.marv.gibbs@gmail.com>
2025-03-27 19:00:32 -0700

    Cleaned up
    Modified 404.php to be friendlier

--- 404.php
+++ 404.php
@@ -9,56 +9,32 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<!-- FEATURE ARCHIVES IMAGE
+	=============================================================================== -->
+	<section class="feature-image feature-image-archives">
+		<h1 class="page-title">Sorry! That page can't be found.</h1>
+	</section>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'madisonriverranch' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'madisonriverranch' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( madisonriverranch_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'madisonriverranch' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'madisonriverranch' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	<div class="container">
+		<div id="primary" class="row">
+			<main id="content" class="col-sm-8">
+				<div class="error-404 not-found">
+					<div class="page-content">
+						<h2>Don't fret! Let's get you back on track.</h2>
+						
+						<!-- NEWSLETTERS
+						=============================================================================== -->
+						<h3>Newsletters</h3>
+						<p>Perhaps you were looking for a newsletter</p>
+						<?php get_template_part( 'template-parts/content', 'newsletters' ); ?>
+						
+						<p>... or , just head back to the <a href="<?php echo esc_url( home_url( '/' ) ); ?>">home page</a>.</p>
+						
+					</div> <!-- .page-content -->
+				</div> <!-- .error-404 -->
+			</main> <!-- #content -->
+		</div> <!-- #primary -->
+	</div> <!-- .container -->
 
 <?php
 get_footer();
