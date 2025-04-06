36937d0bcdc75563353faba89ca229c01aba7b26
Marvin Gibbs <c.marv.gibbs@gmail.com>
2025-04-06 10:55:25 -0700

    Modified functions.php
    Turned off 'quicktags' for the bbPress 'tinymce' editor.

--- functions.php
+++ functions.php
@@ -146,6 +146,7 @@ add_action( 'wp_enqueue_scripts', 'madisonriverranch_scripts' );
 
 function bbp_enable_visual_editor( $args = array() ) {
 	$args['tinymce'] = true;
+	$args['quicktags'] = false;
 	return $args;
 }
 add_filter( 'bbp_after_get_the_content_parse_args', 'bbp_enable_visual_editor' );
