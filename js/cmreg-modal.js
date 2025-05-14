7f5fb1810a1ac5c6c6c71587508595a870e18e9c
Marvin Gibbs <c.marv.gibbs@gmail.com>
2025-05-14 13:51:15 -0700

    Added modal to force login
    Users can request email notification when topics and replies are created. The email link allows them to open the website to that page. These files were added to provide feedback that user must login to view the page.

---
+++ js/cmreg-modal.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const modal = document.getElementById('cmreg-blocker-modal');
+	if (modal) {
+		modal.style.display = 'flex';
+		document.body.style.overflow = 'hidden';
+	}
+});
